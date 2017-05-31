772bfd6dc64a69149b12f51d450b2df9a2882800
udaygin <uday.gottapu.in@gmail.com>
2017-05-31 17:07:07 +0530

    Update mdns.lua

--- mdns.lua
+++ mdns.lua
@@ -3,8 +3,6 @@
     Original work Copyright (c) 2015 Frank Edelhaeuser
     Modified work Copyright (c) 2017 Uday G
     
-    This file is part of lua-mdns.
-
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
